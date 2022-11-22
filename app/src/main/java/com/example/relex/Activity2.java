cc05e3cc0c77aabc578838bf031ff5587fe128da
BriceEyss <brice.lehioui@edu.esiee.fr>
2022-11-22 17:29:28 +0100

    Game One finished, upgrades made. Colors and font of text changed. Buttons dynamism when push

--- app/src/main/java/com/example/relex/Activity2.java
+++ app/src/main/java/com/example/relex/Activity2.java
@@ -16,11 +16,11 @@ public class Activity2 extends AppCompatActivity {
     public RelativeLayout relativeLayout;
 
 
+
     // runnable function
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-
             // set the background on the screen
             relativeLayout.setBackgroundResource(R.color.green);
 
@@ -39,7 +39,8 @@ public class Activity2 extends AppCompatActivity {
                     // when the stop button is clicked
                     long time1 = System.currentTimeMillis();
                     timeAfter = time1*0.001-time*0.001;
-
+                    button2.setVisibility(View.GONE);
+                    button1.setVisibility(View.VISIBLE);
                     if (timeAfter<= 0.5){
                         relativeLayout.setBackgroundResource(0);
                         // display reflex time in toast message
@@ -53,20 +54,19 @@ public class Activity2 extends AppCompatActivity {
                     }else if (timeAfter<= 0.8){
                         relativeLayout.setBackgroundResource(0);
                         // display reflex time in toast message
-                        ((TextView)findViewById(R.id.tvVar2)).setText("Vos reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, c'est bof");
+                        ((TextView)findViewById(R.id.tvVar2)).setText("Tes reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, c'est bof");
                         relativeLayout.setBackgroundResource(R.drawable.mister2);
                     }else if (timeAfter<= 1){
                         relativeLayout.setBackgroundResource(0);
                         // display reflex time in toast message
-                        ((TextView)findViewById(R.id.tvVar2)).setText("Vos reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, t'as fais expres avoue ?");
+                        ((TextView)findViewById(R.id.tvVar3)).setText("Tes reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, t'as fais expres avoue ?");
                         relativeLayout.setBackgroundResource(R.drawable.mister3);
-                    }else if (timeAfter>= 1.25){
+                    }else if (timeAfter> 1){
                         relativeLayout.setBackgroundResource(0);
                         // display reflex time in toast message
-                        ((TextView)findViewById(R.id.tvVar2)).setText("Vos reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, t'es null en sah");
+                        ((TextView)findViewById(R.id.tvVar3)).setText("tes reflex ont prit " + Float.parseFloat(String.format("%.5f",timeAfter)) + " secondes, t'es null en sah");
                         relativeLayout.setBackgroundResource(R.drawable.mister5);
                     }
-
                     // remove the background again
                   //  relativeLayout.setBackgroundResource(0);
                 }
@@ -78,16 +78,23 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        button1 = findViewById(R.id.btVar1);
+        button2 = findViewById(R.id.btVar2);
 
         getSupportActionBar().setTitle("Reflex");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         relativeLayout = findViewById(R.id.rlVar1);
-        button1 = findViewById(R.id.btVar1);
-        button2 = findViewById(R.id.btVar2);
+
+        View b1 = findViewById(R.id.btVar1);
+        View b = findViewById(R.id.btVar2);
+        b.setVisibility(View.GONE);
 
         /* function when the start button is clicked */
         button1.setOnClickListener(view -> {
+            b1.setVisibility(View.GONE);
+            b.setVisibility(View.VISIBLE);
             relativeLayout.setBackgroundResource(0);
+            ((TextView)findViewById(R.id.tvVar3)).setText("");
             ((TextView)findViewById(R.id.tvVar2)).setText("");
             ((TextView)findViewById(R.id.tvVar1)).setText("Cliquez d'abord sur Start, et attendez jusqu'à ce que la couleur de fond change.\n" +
                     "Dès qu'elle change, cliquez sur Stop");
