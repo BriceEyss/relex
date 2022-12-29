b461d54d0bc28dcffaeecee13ac652516c7abc7c
BriceEyss <brice.lehioui@edu.esiee.fr>
2022-12-29 16:52:44 +0100

    Commentaires pour 8balls

--- app/src/main/java/com/example/relex/Activity3.java
+++ app/src/main/java/com/example/relex/Activity3.java
@@ -22,22 +22,22 @@ public class Activity3 extends AppCompatActivity {
         // Permet d'avoir le layout de l'activité3
         setContentView(R.layout.activity_3);
 
-        // link all the variables with its id
+        // lie toutes les variables avec son identifiant
         final ImageView imageView = (ImageView) findViewById(R.id.image_eightBall);
         Button button = (Button) findViewById(R.id.askButton);
 
-        // create an array to store all the images
+        // crée un tableau pour stocker toutes les images
         final int a[] = {R.drawable.ball1,R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
 
-        // ask button's onClick function
+        // fonction onClick du bouton "appuyez"
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // generate a number using Random() function
+                // générer un nombre en utilisant la fonction Random() pour choisir l'image
                 Random random = new Random();
                 int x = random.nextInt(4);
 
-                // set the image to the view
+                // choisi l'image grace au random
                 imageView.setImageResource(a[x]);
             }
         });
